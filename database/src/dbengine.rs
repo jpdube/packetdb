1c1ec58817e0368d62df2e0e1fec69ba6148712f
JP Dube <jpdube@numa-informatique.com>
2024-06-18 10:31:57 -0400

    Implemented parallel search
    - The database is now capable of searching multiple files at a time
    
    - Protocol statistics need to be added in order to find tune the number
      of threads required to efficiently search the db

--- database/src/dbengine.rs
+++ database/src/dbengine.rs
@@ -8,6 +8,7 @@ use crate::query_result::QueryResult;
 
 use anyhow::Result;
 use log::info;
+use rayon::prelude::*;
 use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
@@ -38,38 +39,61 @@ impl DbEngine {
             let mut top_limit: usize = expr.top;
             let mut file_count = 0;
             let top_offset: usize = expr.top + expr.offset;
+            let mut stop = false;
+            let interpreter = Interpreter::new(expr.clone());
+            let chunk_size = 2;
 
             self.offset = 0;
 
-            let mut pkt_index = PktIndex::default();
-            println!("Top: {}, Offset: {}", expr.top, expr.offset);
+            // println!("Top: {}, Offset: {}", expr.top, expr.offset);
             match self.get_index_files() {
                 Ok(pkt_list) => {
-                    for file_id in pkt_list {
-                        file_count += 1;
-                        let interpreter = Interpreter::new(expr.clone());
-                        let ptr = pkt_index.search_index(&expr, file_id);
+                    while !stop {
+                        for chunk_id in pkt_list.chunks(chunk_size) {
+                            file_count += chunk_size;
 
-                        let result = interpreter.run_pgm_seek(&ptr, top_limit);
-                        count += result.len();
-                        top_limit = top_offset - count;
+                            let interp_result: Vec<_> = chunk_id
+                                .into_par_iter()
+                                .map(|file_id| {
+                                    let mut pkt_index = PktIndex::default();
+                                    let ptr = pkt_index.search_index(&expr, *file_id);
 
-                        for r in result {
-                            if self.offset < expr.offset {
-                                self.offset += 1;
-                                continue;
-                            } else {
-                                self.result.add_record(r);
+                                    interpreter.run_pgm_seek(&ptr, top_limit)
+                                })
+                                .collect();
+
+                            for r in &interp_result {
+                                count += r.len();
+                            }
+
+                            top_limit = top_offset - count;
+
+                            let mut result_count = 0;
+                            if count >= top_offset {
+                                println!("len: {}", count);
+                                for c in interp_result {
+                                    for r in c {
+                                        if self.offset < expr.offset {
+                                            self.offset += 1;
+                                            continue;
+                                        } else {
+                                            if result_count < expr.top {
+                                                self.result.add_record(r);
+                                                result_count += 1;
+                                            } else {
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                stop = true;
+                                break;
                             }
                         }
 
-                        if count >= top_offset {
-                            println!("len: {}", count);
-                            break;
-                        }
+                        info!("Nbr files searched: {}", file_count);
                     }
-
-                    info!("Nbr files searched: {}", file_count);
                 }
                 Err(e) => println!("Error with index error:{}", e),
             }
