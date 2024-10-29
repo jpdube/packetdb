822d90a176c410976da1b5ab2980d4a4fa82a6c3
JP Dube <jpdube@numa-informatique.com>
2024-10-29 15:09:48 -0400

    Checking for endianness
    - Nove openting the header and validating the endianness

--- database/src/seek_packet.rs
+++ database/src/seek_packet.rs
@@ -1,3 +1,4 @@
+use byteorder::BigEndian;
 use byteorder::{ByteOrder, LittleEndian};
 use frame::packet::Packet;
 use std::fs::File;
@@ -7,6 +8,9 @@ use std::io::BufReader;
 use crate::config;
 use crate::packet_ptr::PacketPtr;
 
+const HEADER_BE: u32 = 0xa1b2c3d4;
+const HEADER_LE: u32 = 0xd4c3b2a1;
+
 pub struct SeekPacket {
     file: BufReader<File>,
     index: usize,
@@ -14,20 +18,33 @@ pub struct SeekPacket {
     data: Vec<u8>,
     relative_ptr: u64,
     psize: usize,
+    magic_no: u32,
 }
 
 impl SeekPacket {
     pub fn new(packet_list: PacketPtr) -> Self {
         let fname = &format!("{}/{}.pcap", config::CONFIG.db_path, packet_list.file_id);
 
-        Self {
+        let mut magic_file = BufReader::new(File::open(fname).unwrap());
+        let mut gheader = [0; 24];
+        let magic_no: u32;
+        if magic_file.read_exact(&mut gheader).is_ok() {
+            magic_no = BigEndian::read_u32(&gheader[0..4]);
+        } else {
+            magic_no = HEADER_LE
+        }
+
+        let self_conf = Self {
             file: BufReader::new(File::open(fname).unwrap()),
             index: 0,
             plist: packet_list,
             data: Vec::new(),
             relative_ptr: 0,
             psize: 0,
-        }
+            magic_no,
+        };
+
+        self_conf
     }
 
     pub fn next(&mut self) -> Option<Packet> {
@@ -44,7 +61,15 @@ impl SeekPacket {
             return None;
         }
 
-        self.psize = LittleEndian::read_u32(&pheader[12..16]) as usize;
+        //--- Must determine what to do when psize is zero. The file pointer will
+        //--- not advance and we will spin on our self
+        match self.magic_no {
+            HEADER_LE => self.psize = LittleEndian::read_u32(&pheader[12..16]) as usize,
+            HEADER_BE => self.psize = BigEndian::read_u32(&pheader[12..16]) as usize,
+            _ => self.psize = 0,
+        }
+
+        // self.psize = BigEndian::read_u32(&pheader[12..16]) as usize;
         self.data.resize(self.psize, 0);
         self.file.read_exact(&mut self.data).unwrap();
         self.relative_ptr = self.file.stream_position().unwrap();
