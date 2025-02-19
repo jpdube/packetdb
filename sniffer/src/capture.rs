7cd0f5d43da42b7f4b11d5aabdf1ba552e75c93e
JP Dube <jpdube@numa-informatique.com>
2025-02-19 10:55:12 -0500

    Remove packt ptr and code cleanup
    - Remove the clone call when sening a packet to the process
      thread
    
    - Remove the packet ptr variable
    
    - Removed the file_id, pkt_ptr from the PacketRef struct
    
    - Variable pkt assigned from PacketRef thread channel: remove mutability

--- sniffer/src/capture.rs
+++ sniffer/src/capture.rs
@@ -4,7 +4,6 @@ use database::dbconfig::DBConfig;
 use database::index_manager::IndexManager;
 use pcap::Capture;
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::{BufWriter, Write};
 use std::sync::mpsc;
 use std::thread;
@@ -18,8 +17,6 @@ const GLOBAL_HDR: [u8; 24] = [
 
 #[derive(Default, Debug, Clone)]
 struct PacketRef {
-    file_id: u32,
-    pkt_ptr: u32,
     packet: Vec<u8>,
     orig_len: u32,
     cap_len: u32,
@@ -40,15 +37,16 @@ pub fn capture(device_name: &str) -> Result<(), pcap::Error> {
     });
 
     thread::spawn(move || {
-        let mut file_ptr: u32 = 0;
         let mut file_no: u32;
         let mut pkt_count: u32 = 0;
         let mut dbconfig: DBConfig = DBConfig::default();
-        file_no = dbconfig.next_fileid().unwrap();
         let mut header: Vec<u8> = Vec::with_capacity(16);
 
+        file_no = dbconfig.next_fileid().unwrap();
         let mut bin_file =
             BufWriter::new(File::create(format!("{}/{}.pcap", &CONFIG.db_path, file_no)).unwrap());
+        bin_file.write_all(&GLOBAL_HDR).unwrap();
+
         for p in rx_packet {
             if pkt_count >= MAX_PACKETS_PER_FILE {
                 tx_db.send(file_no).unwrap();
@@ -58,19 +56,11 @@ pub fn capture(device_name: &str) -> Result<(), pcap::Error> {
                 bin_file = BufWriter::new(
                     File::create(format!("{}/{}.pcap", &CONFIG.db_path, file_no)).unwrap(),
                 );
-                file_ptr = 0;
-            }
-
-            let mut pkt: PacketRef = p;
-            pkt_count += 1;
-
-            if file_ptr == 0 {
                 bin_file.write_all(&GLOBAL_HDR).unwrap();
             }
 
-            file_ptr = bin_file.stream_position().unwrap() as u32;
-            pkt.file_id = file_no;
-            pkt.pkt_ptr = file_ptr;
+            let pkt: PacketRef = p;
+            pkt_count += 1;
 
             header.clear();
             header.write_u32::<LittleEndian>(pkt.timestamp).unwrap();
@@ -83,7 +73,7 @@ pub fn capture(device_name: &str) -> Result<(), pcap::Error> {
         }
     });
 
-    println!("Starting capture on interface");
+    println!("Starting capture on interface: {}", device_name);
     let mut cap = Capture::from_device(device_name)
         .unwrap()
         .promisc(true)
@@ -98,8 +88,6 @@ pub fn capture(device_name: &str) -> Result<(), pcap::Error> {
             timestamp: packet.header.ts.tv_sec as u32,
             ts_us: packet.header.ts.tv_usec as u32,
             packet: packet.data.to_vec(),
-            file_id: 0,
-            pkt_ptr: 0,
         };
 
         let _ = tx_packet.send(pkt).unwrap();
