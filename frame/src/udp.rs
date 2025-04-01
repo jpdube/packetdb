c3f5ab2e7b372843a2305586708f9ced4154aea2
JP Dube <jpdube@numa-informatique.com>
2025-04-01 14:24:29 -0400

    UDP using new to instantiate
    - To standardize layers the set_packet was replaced by the new fn

--- frame/src/udp.rs
+++ frame/src/udp.rs
@@ -3,25 +3,22 @@ use crate::pfield::{Field, FieldType};
 use crate::{fields, packet_display::PacketDisplay};
 use byteorder::{BigEndian, ByteOrder};
 
-const _UDP_HEADER_LEN: u8 = 8;
+const UDP_HEADER_LEN: usize = 8;
 
 #[derive(Debug, Default, Clone)]
 pub struct UdpFrame<'a> {
     raw_packet: &'a [u8],
-    // raw_packet: Vec<u8>,
 }
 
 impl<'a> UdpFrame<'a> {
     pub fn new(packet: &'a [u8]) -> Self {
         Self { raw_packet: packet }
     }
-    // pub fn set_packet(&mut self, packet: Vec<u8>) {
-    //     self.raw_packet = packet;
-    // }
 
     pub fn dport(&self) -> u16 {
         BigEndian::read_u16(&self.raw_packet[2..4])
     }
+
     pub fn sport(&self) -> u16 {
         BigEndian::read_u16(&self.raw_packet[0..2])
     }
@@ -34,8 +31,12 @@ impl<'a> UdpFrame<'a> {
         BigEndian::read_u16(&self.raw_packet[6..8])
     }
 
-    pub fn _payload(&self) -> Vec<u8> {
-        self.raw_packet[8..].to_vec()
+    pub fn header_len(&self) -> usize {
+        UDP_HEADER_LEN
+    }
+
+    pub fn payload(&self) -> Vec<u8> {
+        self.raw_packet[UDP_HEADER_LEN..].to_vec()
     }
 
     pub fn payload_range(&self, offset: usize, len: usize) -> Vec<u8> {
@@ -113,13 +114,12 @@ mod tests {
             0x7f, 0x7f, 0x7e, 0x7d, 0x7d, 0x7d, 0x7e, 0xff, 0xff, 0xff, 0xff, 0xfe,
         ];
 
-        let mut pkt = UdpFrame::default();
-        pkt.set_packet(packet);
+        let pkt = UdpFrame::new(&packet);
 
         assert_eq!(pkt.length(), 0x00b4, "UDP length");
         assert_eq!(
-            pkt._payload().len(),
-            0x00b4 - _UDP_HEADER_LEN as usize,
+            pkt.payload().len(),
+            0x00b4 - UDP_HEADER_LEN as usize,
             "UDP length in payload"
         );
     }
@@ -142,8 +142,7 @@ mod tests {
             0x7f, 0x7f, 0x7e, 0x7d, 0x7d, 0x7d, 0x7e, 0xff, 0xff, 0xff, 0xff, 0xfe,
         ];
 
-        let mut pkt = UdpFrame::default();
-        pkt.set_packet(packet);
+        let pkt = UdpFrame::new(&packet);
 
         assert_eq!(pkt.checksum(), 0x7915, "UDP checksum");
     }
@@ -152,8 +151,7 @@ mod tests {
     fn udp_frame_sport() {
         let packet: Vec<u8> = vec![0x2f, 0xd2, 0x40, 0x16, 0x00, 0xb4, 0x79, 0x15];
 
-        let mut pkt = UdpFrame::default();
-        pkt.set_packet(packet);
+        let pkt = UdpFrame::new(&packet);
 
         assert_eq!(pkt.sport(), 0x2fd2, "UDP source port");
     }
@@ -162,8 +160,7 @@ mod tests {
     fn udp_frame_dport() {
         let packet: Vec<u8> = vec![0x2f, 0xd2, 0x40, 0x16, 0x00, 0xb4, 0x79, 0x15];
 
-        let mut pkt = UdpFrame::default();
-        pkt.set_packet(packet);
+        let pkt = UdpFrame::new(&packet);
 
         assert_eq!(pkt.dport(), 0x4016, "UDP destination port");
     }
