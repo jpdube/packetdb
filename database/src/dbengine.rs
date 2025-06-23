1387696150abe7c9eb9093c2f31c138f1236b3a4
JP Dube <jpdube@numa-informatique.com>
2025-06-23 10:26:06 -0400

    Removed chunks and parallel search, converted to sequential
    - Removed all the code for chunks stats
    
    - Removed all the code for parallel search now using
      sequential search

--- database/src/dbengine.rs
+++ database/src/dbengine.rs
@@ -1,7 +1,7 @@
 use crate::config::CONFIG;
 use crate::cursor::Cursor;
 use crate::exec_plan::ExecutionPlan;
-use crate::index_manager::{IndexManager, ProtoStat};
+use crate::index_manager::{IndexField, IndexManager, ProtoStat};
 use crate::interpreter::Interpreter;
 use crate::parse::{Parse, PqlStatement};
 use crate::pcapfile::PcapFile;
@@ -10,7 +10,6 @@ use frame::packet::Packet;
 
 use anyhow::Result;
 use log::{debug, info};
-use rayon::prelude::*;
 use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
@@ -30,12 +29,15 @@ impl DbEngine {
     }
 
     pub fn run(&mut self, query: &str) -> Result<Cursor, String> {
+        let pidx = IndexManager::default();
+        pidx.read_proto_index();
+
         println!("Searching for: {}", query);
         let mut parse = Parse::new();
 
         self.exec_plan.start("Start search");
         if let Ok(expr) = parse.parse_select(query) {
-            debug!("--> Select query: {}", expr);
+            debug!("--> Select query: {:?}", expr.search_type);
 
             let mut query_result = QueryResult::new(expr.clone());
 
@@ -44,7 +46,6 @@ impl DbEngine {
 
             self.offset = 0;
             println!("Chunk size: {}", self.chunk_size(&expr));
-            let nbr_cores = self.chunk_size(&expr);
 
             if expr.has_id_search() {
                 debug!("In ID search");
@@ -57,29 +58,24 @@ impl DbEngine {
                 self.exec_plan.show();
                 Ok(query_result.get_result())
             } else {
-                match self.get_index_files() {
+                if expr.search_type.contains(&IndexField::Dhcp) {
+                    info!("::::::::: FOUND DHCP INDEX ::::::::::::");
+                }
+                let files_list = self.get_index_files();
+                match files_list {
+                    // match self.get_index_files() {
                     Ok(pkt_list) => {
                         while !query_result.count_reach() {
-                            for chunk_id in pkt_list.chunks(nbr_cores) {
-                                file_count += nbr_cores;
+                            file_count += 1;
 
-                                let interp_result: Vec<_> = chunk_id
-                                    .into_par_iter()
-                                    .map(|file_id| {
-                                        let mut pkt_index = IndexManager::default();
-                                        let ptr = pkt_index.search_index(&expr, *file_id);
+                            for file_id in &pkt_list {
+                                let mut pkt_index = IndexManager::default();
+                                let ptr = pkt_index.search_index(&expr, *file_id);
 
-                                        interpreter.run_pgm_seek(&ptr, expr.top)
-                                    })
-                                    .collect();
+                                let c = interpreter.run_pgm_seek(&ptr, expr.top);
 
-                                for c in interp_result {
-                                    for r in c {
-                                        query_result.add(r);
-                                        if query_result.count_reach() {
-                                            break;
-                                        }
-                                    }
+                                for r in c {
+                                    query_result.add(r);
                                     if query_result.count_reach() {
                                         break;
                                     }
@@ -88,8 +84,8 @@ impl DbEngine {
                                     break;
                                 }
                             }
-                            info!("Nbr files searched: {}", file_count);
                         }
+                        info!("Nbr files searched: {}", file_count);
                     }
                     Err(e) => println!("Error with index error:{}", e),
                 }
@@ -103,6 +99,87 @@ impl DbEngine {
         }
     }
 
+    // pub fn run(&mut self, query: &str) -> Result<Cursor, String> {
+    //     let pidx = IndexManager::default();
+    //     pidx.read_proto_index();
+
+    //     println!("Searching for: {}", query);
+    //     let mut parse = Parse::new();
+
+    //     self.exec_plan.start("Start search");
+    //     if let Ok(expr) = parse.parse_select(query) {
+    //         debug!("--> Select query: {:?}", expr.search_type);
+
+    //         let mut query_result = QueryResult::new(expr.clone());
+
+    //         let mut file_count = 0;
+    //         let interpreter = Interpreter::new(expr.clone());
+
+    //         self.offset = 0;
+    //         println!("Chunk size: {}", self.chunk_size(&expr));
+    //         let nbr_cores = self.chunk_size(&expr);
+
+    //         if expr.has_id_search() {
+    //             debug!("In ID search");
+    //             let pkt_result = self.get_id_packets(expr.id_search);
+    //             for p in pkt_result {
+    //                 query_result.add(p);
+    //             }
+
+    //             self.exec_plan.stop();
+    //             self.exec_plan.show();
+    //             Ok(query_result.get_result())
+    //         } else {
+    //             if expr.search_type.contains(&IndexField::Dhcp) {
+    //                 info!("::::::::: FOUND DHCP INDEX ::::::::::::");
+    //             }
+    //             let files_list = self.get_index_files();
+    //             match files_list {
+    //                 // match self.get_index_files() {
+    //                 Ok(pkt_list) => {
+    //                     while !query_result.count_reach() {
+    //                         for chunk_id in pkt_list.chunks(nbr_cores) {
+    //                             file_count += nbr_cores;
+
+    //                             let interp_result: Vec<_> = chunk_id
+    //                                 .into_par_iter()
+    //                                 .map(|file_id| {
+    //                                     let mut pkt_index = IndexManager::default();
+    //                                     let ptr = pkt_index.search_index(&expr, *file_id);
+
+    //                                     interpreter.run_pgm_seek(&ptr, expr.top)
+    //                                 })
+    //                                 .collect();
+
+    //                             for c in interp_result {
+    //                                 for r in c {
+    //                                     query_result.add(r);
+    //                                     if query_result.count_reach() {
+    //                                         break;
+    //                                     }
+    //                                 }
+    //                                 if query_result.count_reach() {
+    //                                     break;
+    //                                 }
+    //                             }
+    //                             if query_result.count_reach() {
+    //                                 break;
+    //                             }
+    //                         }
+    //                         info!("Nbr files searched: {}", file_count);
+    //                     }
+    //                 }
+    //                 Err(e) => println!("Error with index error:{}", e),
+    //             }
+
+    //             self.exec_plan.stop();
+    //             self.exec_plan.show();
+    //             Ok(query_result.get_result())
+    //         }
+    //     } else {
+    //         Err(String::from("Error reading database"))
+    //     }
+    // }
     fn get_id_packets(&self, id_list: Vec<u64>) -> Vec<Packet> {
         let mut result: Vec<Packet> = Vec::new();
 
