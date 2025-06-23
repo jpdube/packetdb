5d263665e255bdb1775dff3105a973a0a316b962
JP Dube <jpdube@numa-informatique.com>
2025-06-23 11:25:18 -0400

    Empty index folders
    - Can now empty index folders

--- database/src/file_manager.rs
+++ database/src/file_manager.rs
@@ -3,44 +3,61 @@ use std::fs;
 
 use crate::config::CONFIG;
 
-#[derive(Default, Debug)]
-pub struct FileManager {}
+pub fn clean_indexes() {
+    clean_index();
+    clean_proto_index();
+}
 
-impl FileManager {
-    pub fn proto_index_filename(&self, file_id: u32, proto_id: u32) -> String {
-        format!(
-            "{}/{}_{:x}.pidx",
-            &CONFIG.proto_index_path, file_id, proto_id
-        )
+pub fn clean_proto_index() {
+    clean_folder(&CONFIG.proto_index_path);
+}
+
+pub fn clean_index() {
+    clean_folder(&CONFIG.index_path);
+}
+
+pub fn clean_folder(folder_name: &str) {
+    let paths: Vec<_> = fs::read_dir(&folder_name)
+        .unwrap()
+        .map(|r| r.unwrap())
+        .collect();
+
+    for path in paths {
+        println!("Deleting: {:?}", path.path());
+        fs::remove_file(&path.path()).unwrap();
     }
+}
+pub fn proto_index_filename(file_id: u32, proto_id: u32) -> String {
+    format!(
+        "{}/{}_{:x}.pidx",
+        &CONFIG.proto_index_path, file_id, proto_id
+    )
+}
 
-    pub fn index_filename(&self, file_id: u32) -> String {
-        format!("{}/{}.pidx", &CONFIG.index_path, file_id)
-    }
+pub fn index_filename(file_id: u32) -> String {
+    format!("{}/{}.pidx", &CONFIG.index_path, file_id)
+}
 
-    pub fn empty_proto_index(&self) {}
+pub fn proto_index_list() -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
 
-    pub fn proto_index_list(&self) -> Vec<String> {
-        let mut result: Vec<String> = Vec::new();
-
-        let mut paths: Vec<_> = fs::read_dir(&CONFIG.index_path)
+    let mut paths: Vec<_> = fs::read_dir(&CONFIG.index_path)
+        .unwrap()
+        .map(|r| r.unwrap())
+        .collect();
+    paths.sort_by_key(|dir| {
+        FileTime::from_creation_time(&dir.metadata().unwrap())
             .unwrap()
-            .map(|r| r.unwrap())
-            .collect();
-        paths.sort_by_key(|dir| {
-            FileTime::from_creation_time(&dir.metadata().unwrap())
-                .unwrap()
-                .nanoseconds()
-        });
-        for path in paths {
-            println!(
-                "Name: {} -> {:?}",
-                path.path().display(),
-                FileTime::from_last_modification_time(&path.metadata().unwrap()).nanoseconds()
-            );
-            result.push(path.file_name().into_string().unwrap());
-        }
-
-        result
+            .nanoseconds()
+    });
+    for path in paths {
+        println!(
+            "Name: {} -> {:?}",
+            path.path().display(),
+            FileTime::from_last_modification_time(&path.metadata().unwrap()).nanoseconds()
+        );
+        result.push(path.file_name().into_string().unwrap());
     }
+
+    result
 }
