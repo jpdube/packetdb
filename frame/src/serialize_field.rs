b5fb499bc54b4b896acec7a1b87e94c232babb7b
JP Dube <jpdube@numa-informatique.com>
2025-11-05 18:20:43 -0500

    Renamed the trait to SerializeField

---
+++ frame/src/serialize_field.rs
@@ -0,0 +1,10 @@
+pub trait SerializeField {
+    fn field_to_binary(&self) -> Vec<u8>;
+    fn field_def_to_binary(&self) -> Vec<u8>;
+    fn from_binary_to_field(
+        field_type: u16,
+        field_len: u16,
+        field_name: String,
+        value: Vec<u8>,
+    ) -> Self;
+}
--- frame/src/to_binary.rs
+++
@@ -1,4 +0,0 @@
-pub trait ToBinary {
-    fn field_to_binary(&self) -> Vec<u8>;
-    fn field_def_to_binary(&self) -> Vec<u8>;
-}
